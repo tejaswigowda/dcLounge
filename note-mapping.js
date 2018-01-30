fb7c3f05ac841c167ae4b3604b06da2cf76da3c6
student <tejaswi@asu.edu>
2018-01-30 12:27:24 -0700

    ac

--- note-mapping.js
+++ note-mapping.js
@@ -5,9 +5,9 @@ autowatch = 1;
 function note(note, velocity) {
 	var pitch = note % 12;
 	var octave = Math.floor(note / 12);
-	//post(pitch, octave, "\n");
+	post(pitch, octave, note, velocity,"\n");
 	
-	outlet( 0, (pitch % 4) << 2, octave * 23, 255 - (pitch * 23));
+	outlet( 0, (note*octave % 4) << 2, velocity * 23, 255 - (pitch * 23));
 
 
 }
